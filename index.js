ac63d194f5874a9d474ff6e0f92e7838fc3f9468
Afolabi Opeyemi <yemijoshua81@gmail.com>
2022-09-19 11:29:24 +0100

    fixing production

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use('/api', require("./routes/route"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get('/*', function (req, res) {
+  app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 } else {
